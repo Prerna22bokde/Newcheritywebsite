641e38467c045e4d38478043806ae5f1a6d67c45
prerna <bokdeprerna22@gmail.comgit config --global user.email bokdeprerna22@gmail.com>
2024-06-24 23:59:44 +0530

    changes

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../../component/Layout";
 import Home from "../Home/Home/Home";
+import Formpage from "./Formpage";
+import Footer from "../../component/Footer";
 
 export default function Contact() {
   return (
     <Layout>
       <Home />
+      <Formpage />
+      <Footer />
     </Layout>
   );
 }
--- src/components/Pages/Contact/Formpage.jsx
+++ src/components/Pages/Contact/Formpage.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import icon from "../../Images/error_icon.png";
+import "./Contact.css";
 
 export default function Formpage() {
-  return <div></div>;
+  return (
+    <>
+      <div className="formpage">
+        <div className="form">
+          <input className="form_entry" placeholder="Your Name" type="text" />
+          <input className="form_entry" placeholder="Your Email" type="email" />
+          <input className="form_entry" placeholder="Subject" type="text" />
+          <textarea
+            className="form_entry"
+            placeholder="Message"
+            type="text"
+            rows={10}
+            cols={10}
+          />
+          <button>SEND MESSAGE</button>
+        </div>
+        <div className="text-form">
+          <div className="text-box">
+            <img src={icon} alt="error_icon" />
+            <h3>Sorry! Something went wrong</h3>
+            <p>
+              This page didn't Google Maps correctly. See the Javascript console
+              for technical details.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
